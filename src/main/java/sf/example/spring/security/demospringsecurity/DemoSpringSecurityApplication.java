52251925c4bd487dff98ab1ab26a262e1111691a
Saber FRAJ <frejsaber@gmail.com>
2018-12-01 00:32:39 +0100

    first commit

---
+++ src/main/java/sf/example/spring/security/demospringsecurity/DemoSpringSecurityApplication.java
@@ -0,0 +1,12 @@
+package sf.example.spring.security.demospringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringSecurityApplication.class, args);
+	}
+}
